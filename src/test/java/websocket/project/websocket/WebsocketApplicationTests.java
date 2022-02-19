60967be100e9369cf7994cea6b27278dd8985465
alissonbk <alissonbk2010@hotmail.com>
2022-02-19 14:51:09 -0300

    first commit

---
+++ src/test/java/websocket/project/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package websocket.project.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
